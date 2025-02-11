a99313914254aa54388cfd7c7629c91ee3ec47ba
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-11 20:26:03 +0530

    [Pragya] Solution of Find Subset

--- Day02/src/main/java/com/setinterface/findsubset/Main.java
+++ Day02/src/main/java/com/setinterface/findsubset/Main.java
@@ -1,4 +1,21 @@
 package com.setinterface.findsubset;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        // Example sets
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        // Add into set1
+        set1.add(2);
+        set1.add(3);
+        // Add into set2
+        set2.add(1);
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+
+        // Check if either set1 is a subset of set2 or set2 is a subset of set1
+        boolean result = Subset.isSubset(set1, set2);
+        System.out.println("Is Subset : " + result);
+    }
 }
--- Day02/src/main/java/com/setinterface/findsubset/Subset.java
+++ Day02/src/main/java/com/setinterface/findsubset/Subset.java
@@ -1,4 +1,10 @@
 package com.setinterface.findsubset;
 
+import java.util.Set;
+// Program to Check if one set is a subset of another.
 public class Subset {
+    // Method to check one set is subset of another or not
+    public static <T> boolean isSubset(Set<T> set1 , Set <T> set2){
+        return set1.containsAll(set2) || set2.containsAll(set1);
+    }
 }
