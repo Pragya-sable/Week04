a6d74ac5fda3159461ae32ea9b2427430dd96460
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-11 20:26:53 +0530

    [Pragya] Solution of Sets are Equal or not

--- Day02/src/main/java/com/setinterface/checktwosetsareequal/CompareSet.java
+++ Day02/src/main/java/com/setinterface/checktwosetsareequal/CompareSet.java
@@ -1,4 +1,9 @@
 package com.setinterface.checktwosetsareequal;
-
+import java.util.Set;
+// Program to compare two sets and determine if they contain the same elements, regardless of order
 public class CompareSet {
+    // Method to check if twp set equal or not
+    public static <T> boolean areSetsEqual(Set<T>set1, Set<T>set2){
+        return set1.equals(set2);
+    }
 }
--- Day02/src/main/java/com/setinterface/checktwosetsareequal/Main.java
+++ Day02/src/main/java/com/setinterface/checktwosetsareequal/Main.java
@@ -1,4 +1,23 @@
 package com.setinterface.checktwosetsareequal;
-
+import java.util.Set;
+import java.util.HashSet;
 public class Main {
+    public static void main(String[] args) {
+        // Input
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        // Add into set1
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        // Add into set2
+        set2.add(4);
+        set2.add(2);
+        set2.add(1);
+
+        boolean result = CompareSet.areSetsEqual(set1, set2);
+        System.out.println("Are the sets equal :  " + result);
+    }
 }
