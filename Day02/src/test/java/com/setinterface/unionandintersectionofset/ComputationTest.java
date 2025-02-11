5dcd67eec54c82cfd8cc9919ab4d08e87c53aeea
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-12 00:46:52 +0530

    [Pragya] Test of Find Union and Intersection of set

--- Day02/src/test/java/com/setinterface/unionandintersectionofset/ComputationTest.java
+++ Day02/src/test/java/com/setinterface/unionandintersectionofset/ComputationTest.java
@@ -1,4 +1,36 @@
 package com.setinterface.unionandintersectionofset;
 
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ComputationTest {
+
+    @Test
+    public void testUnionOfSets() {
+        // Test union of two sets
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+
+        // Union of set1 and set2
+        Set<Integer> result = Computation.unionOfSets(set1, set2);
+        Set<Integer> expected = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        assertEquals(expected, result, "The union of the sets is incorrect.");
+    }
+
+    @Test
+    public void testIntersectionOfSets() {
+        // Test intersection of two sets
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+
+        // Intersection of set1 and set2
+        Set<Integer> result = Computation.intersectionOfSets(set1, set2);
+        Set<Integer> expected = new HashSet<>(Arrays.asList(3));
+
+        assertEquals(expected, result, "The intersection of the sets is incorrect.");
+    }
+
 }
