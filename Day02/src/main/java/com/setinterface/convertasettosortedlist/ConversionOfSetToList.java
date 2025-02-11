a517bdb14f0350922743d66348ead4ae67f0784d
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-11 20:28:43 +0530

    [Pragya] Solution of Conversion of Set to Sorted List

--- Day02/src/main/java/com/setinterface/convertasettosortedlist/ConversionOfSetToList.java
+++ Day02/src/main/java/com/setinterface/convertasettosortedlist/ConversionOfSetToList.java
@@ -1,4 +1,16 @@
 package com.setinterface.convertasettosortedlist;
 
+import java.util.*;
+
+// Program to Convert a HashSet of integers into a sorted list in ascending order
 public class ConversionOfSetToList {
+    // Method to sort the set
+    public static List<Integer> convertSetToList(Set<Integer> set){
+        // Conversion of hashset to list
+        List<Integer> sortedList = new ArrayList<>(set);
+
+        // Sort the list
+        Collections.sort(sortedList);
+        return sortedList;
+    }
 }
--- Day02/src/main/java/com/setinterface/convertasettosortedlist/Main.java
+++ Day02/src/main/java/com/setinterface/convertasettosortedlist/Main.java
@@ -1,4 +1,19 @@
 package com.setinterface.convertasettosortedlist;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
 public class Main {
+    public static void main(String[] args) {
+            // Input sets
+            Set<Integer> set = new HashSet<>();
+            // Add in set1
+            set.add(11);
+            set.add(2);
+            set.add(3);
+            set.add(0);
+
+        // Convert to sorted list
+        List<Integer> sortedList = ConversionOfSetToList.convertSetToList(set);
+        System.out.println("Sorted List: " + sortedList);
+        }
 }
