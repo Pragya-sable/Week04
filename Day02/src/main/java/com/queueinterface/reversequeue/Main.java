aeb81027092cf977e5235ad588d1ac5ab950f7f8
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-11 21:30:42 +0530

    [Pragya] Solution of Reverse a Queue

--- Day02/src/main/java/com/queueinterface/reversequeue/Main.java
+++ Day02/src/main/java/com/queueinterface/reversequeue/Main.java
@@ -1,4 +1,22 @@
 package com.queueinterface.reversequeue;
-
+import java.util.Queue;
+import java.util.LinkedList;
 public class Main {
+    public static void main(String[] args) {
+        // Create a Queue and add some elements
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        // Print original queue
+        System.out.println("Original Queue: " + queue);
+
+        // Reverse the queue
+        Reverse.reverseQueue(queue);
+
+        // Print the reversed queue
+        System.out.println("Reversed Queue: " + queue);
+    }
+
 }
--- Day02/src/main/java/com/queueinterface/reversequeue/Reverse.java
+++ Day02/src/main/java/com/queueinterface/reversequeue/Reverse.java
@@ -1,4 +1,20 @@
 package com.queueinterface.reversequeue;
 
+import java.util.Queue;
+// Program to reverse the Queue
 public class Reverse {
+    // Generic method to reverse the queue
+    public static <T> void reverseQueue(Queue<T> queue){
+        // Base Case
+        if(queue.isEmpty())
+            return;
+        // Remove the front element
+        T front = queue.remove();
+
+        //Recursive call for reverse the remaining queue
+        reverseQueue(queue);
+
+        // Add the removed element to rear
+        queue.add(front);
+    }
 }
