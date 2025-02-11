fab3c154659783a39d138e3bf72d4b02991c3af6
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-11 20:25:23 +0530

    [Pragya] Solution of Symmetric Difference

--- Day02/src/main/java/com/setinterface/symmetricdifference/Main.java
+++ Day02/src/main/java/com/setinterface/symmetricdifference/Main.java
@@ -1,4 +1,22 @@
 package com.setinterface.symmetricdifference;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        // Input sets
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        // Add in set1
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        // Add in set2
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+
+        // Compute the symmetric difference
+        Set<Integer> symmetricDifferenceResult = SetOperation.symmetricDifference(set1, set2);
+        System.out.println("Symmetric Difference: " + symmetricDifferenceResult);
+    }
 }
+
--- Day02/src/main/java/com/setinterface/symmetricdifference/SetOperation.java
+++ Day02/src/main/java/com/setinterface/symmetricdifference/SetOperation.java
@@ -1,4 +1,22 @@
 package com.setinterface.symmetricdifference;
 
+import java.util.HashSet;
+import java.util.Set;
+// Program to compute Symmetric Difference
 public class SetOperation {
+    // Generic Method to find Symmetric Difference
+    public static <T> Set<T> symmetricDifference(Set<T> set1, Set<T> set2){
+        // Create two set and start with copy of set1
+        Set<T> union = new HashSet<>(set1);
+        Set<T> intersection = new HashSet<>(set1);
+        // Union of set1 and set2
+        union.addAll(set2);
+
+        // Intersection of set1 and set2
+        intersection.retainAll(set2);
+
+        // Symmetric difference = Union - Intersection
+        union.removeAll(intersection);
+        return union;
+    }
 }
