c96e1738eef318ac7f67606cc813aae463e6da30
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-12 00:41:11 +0530

    [Pragya] Test of Implementation of Stack Using Queue

--- Day02/src/test/java/com/queueinterface/implementstackusingqueue/StackUsingQueuesTest.java
+++ Day02/src/test/java/com/queueinterface/implementstackusingqueue/StackUsingQueuesTest.java
@@ -1,4 +1,50 @@
 package com.queueinterface.implementstackusingqueue;
 
-public class StackTest {
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackUsingQueuesTest {
+
+    @Test
+    public void testPushPop() {
+        StackUsingQueues<Integer> stack = new StackUsingQueues<>();
+
+        // Push elements to the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        // Test the top element after pushing
+        assertEquals(3, stack.top(), "Top element should be 3");
+
+        // Pop the top element and test
+        stack.pop();
+        assertEquals(2, stack.top(), "Top element should be 2");
+
+        stack.pop();
+        assertEquals(1, stack.top(), "Top element should be 1");
+
+        stack.pop();
+        assertNull(stack.top(), "Top element should be null when stack is empty");
+    }
+
+    @Test
+    public void testSize() {
+        StackUsingQueues<Integer> stack = new StackUsingQueues<>();
+
+        // Size should be 0 initially
+        assertEquals(0, stack.size(), "Stack size should be 0");
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        // Size should be 3 after pushing 3 elements
+        assertEquals(3, stack.size(), "Stack size should be 3");
+
+        stack.pop();
+        // Size should be 2 after popping 1 element
+        assertEquals(2, stack.size(), "Stack size should be 2");
+    }
 }
