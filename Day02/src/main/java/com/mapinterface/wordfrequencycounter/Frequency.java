1a551e74e1831e566b9fc3955d1320ab54e70a1e
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-12 00:33:29 +0530

    [Pragya] Solution of Word frequency counter

--- Day02/src/main/java/com/mapinterface/wordfrequencycounter/Frequency.java
+++ Day02/src/main/java/com/mapinterface/wordfrequencycounter/Frequency.java
@@ -1,4 +1,28 @@
 package com.mapinterface.wordfrequencycounter;
 
+import java.util.HashMap;
+import java.util.*;
+
 public class Frequency {
+    // Generic method for counting the frequency of element in list
+    public static <T> Map<T, Integer> countFrequency(String input) {
+        // Create map for storing the result
+        Map<T, Integer> map = new HashMap<>();
+
+        // Convert the input text to lowercase for case-insensitivity
+        input = input.toLowerCase();
+
+        // Split the input text into words by spaces
+        String[] words = input.split("[^a-zA-Z]+");
+
+        // Iterate over each word and update the frequency in the map
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                T key = (T) word;
+                map.put(key, map.getOrDefault(key, 0) + 1);
+            }
+
+        }
+        return map;
+    }
 }
--- Day02/src/main/java/com/mapinterface/wordfrequencycounter/Main.java
+++ Day02/src/main/java/com/mapinterface/wordfrequencycounter/Main.java
@@ -1,4 +1,14 @@
 package com.mapinterface.wordfrequencycounter;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        // Take input
+        String inputText = "Hello world, hello Java! Hello world.";
+
+        // Call the generic method to count word frequencies
+        Map<String, Integer> result = Frequency.countFrequency(inputText);
+
+        // Print the word frequency map
+        System.out.println(result);
+    }
 }
