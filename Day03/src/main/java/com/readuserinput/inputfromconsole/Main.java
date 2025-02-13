857b8fd9a658f89b4ae6af2a2954f8e2c2c73345
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-13 15:17:43 +0530

    [Pragya] Solution of Read user input from console

--- Day03/src/main/java/com/readuserinput/inputfromconsole/Main.java
+++ Day03/src/main/java/com/readuserinput/inputfromconsole/Main.java
@@ -1,4 +1,19 @@
+
 package com.readuserinput.inputfromconsole;
 
 public class Main {
+    public static void main(String[] args) {
+        // Get the user info
+        String[] userInformation = ReadUserInputFromConsoleExample.getUserInput();
+
+        // Extract the information from the array
+        String name = userInformation[0];
+        int age = Integer.parseInt(userInformation[1]);
+        String favoriteLanguage = userInformation[2];
+
+        // Save to file
+        ReadUserInputFromConsoleExample.copyToFile(name,age,favoriteLanguage);
+
+    }
 }
+
--- Day03/src/main/java/com/readuserinput/inputfromconsole/ReadUserInputFromConsoleExample.java
+++ Day03/src/main/java/com/readuserinput/inputfromconsole/ReadUserInputFromConsoleExample.java
@@ -1,4 +1,58 @@
 package com.readuserinput.inputfromconsole;
 
+import java.io.*;
+// Program that asks the user for their name, age, and favorite
+// programming language, then saves this information into a file
 public class ReadUserInputFromConsoleExample {
+
+    // Method for taking the user input from console
+    public static String[] getUserInput(){
+        // Create BufferedReader object for taking input
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // Initialize Variable
+        String name = "";
+        int age = 0;
+        String favoriteLanguage = "";
+
+        try{
+            // Enter name
+            System.out.println("Enter name : ");
+            name = bufferedReader.readLine();
+
+            // Enter age
+            System.out.println("Enter age : ");
+            age = Integer.parseInt(bufferedReader.readLine());
+
+            // Enter language
+            System.out.println("Enter favorite Language : ");
+            favoriteLanguage = bufferedReader.readLine();
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+        // return
+        return new String[]{name, String.valueOf(age),favoriteLanguage};
+    }
+
+    // Method to save the input taken from user into a file
+    public static void copyToFile(String name, int age, String favoriteLanguage){
+
+        try(FileWriter writer = new FileWriter("D:/Capgemini Training/output2.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(writer)){
+
+            // Write data to the file
+            bufferedWriter.write("Name : "+name);
+            bufferedWriter.newLine();
+            bufferedWriter.write("Age : "+age);
+            bufferedWriter.newLine();
+            bufferedWriter.write("Language : "+favoriteLanguage);
+
+            System.out.println("Information added successfully");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
+
