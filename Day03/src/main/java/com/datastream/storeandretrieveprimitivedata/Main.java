1588fbfff00eb2742900d66d14d4a41004566964
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-13 15:23:13 +0530

    [Pragya] Solution of Store and retrieve primitive data

--- Day03/src/main/java/com/datastream/storeandretrieveprimitivedata/Main.java
+++ Day03/src/main/java/com/datastream/storeandretrieveprimitivedata/Main.java
@@ -1,4 +1,35 @@
 package com.datastream.storeandretrieveprimitivedata;
 
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Store details in binary file
+        try(DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream("D:/Capgemini Training/Source.txt"))) {
+            Student student1 = new Student(101,"Pragya", 9.17);
+            Student student2 = new Student(102, "Riya",9.5);
+
+            // Write student data to the file
+            student1.writeStudentData(dataOutputStream);
+            student2.writeStudentData(dataOutputStream);
+
+            System.out.println("Details written in the files");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        // Read details from binary file
+        try(DataInputStream dataInputStream = new DataInputStream(new FileInputStream("D:/Capgemini Training/Source.txt"))){
+            // Reading two students from the file
+            Student student1 = Student.readStudentData(dataInputStream);
+            Student student2 = Student.readStudentData(dataInputStream);
+
+            // Print retrieved student details
+            System.out.println("Retrieved Student 1: " + student1.getRollNumber() + ", " + student1.getName() + ", " + student1.getGPA());
+            System.out.println("Retrieved Student 2: " + student2.getRollNumber() + ", " + student2.getName() + ", " + student2.getGPA());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- Day03/src/main/java/com/datastream/storeandretrieveprimitivedata/Student.java
+++ Day03/src/main/java/com/datastream/storeandretrieveprimitivedata/Student.java
@@ -1,4 +1,70 @@
 package com.datastream.storeandretrieveprimitivedata;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+// Program that reads a text file and writes its contents into another file,
+// converting all uppercase letters to lowercase
 public class Student {
+
+    // Field
+    private int rollNumber;
+    private String name;
+    private double GPA;
+
+    // Constructor
+    public Student(int rollNumber,String name, double GPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.GPA = GPA;
+    }
+
+    // Getter and Setter Method
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Method to write data into a file
+    public void writeStudentData(DataOutputStream dataOutputStream)throws IOException{
+        // Write the rollNumber into the file
+        dataOutputStream.writeInt(rollNumber);
+        // Write the name into the file
+        dataOutputStream.writeUTF(name);
+        // Write the GPA into the file
+        dataOutputStream.writeDouble(GPA);
+    }
+
+    // Method to read data from file
+    public static Student readStudentData(DataInputStream dataInputStream)throws IOException{
+        // Read roll number
+        int rollNumber = dataInputStream.readInt();
+        // Read name
+        String name = dataInputStream.readUTF();
+        // Read GPA
+        double GPA = dataInputStream.readDouble();
+
+        return new Student(rollNumber, name, GPA);
+    }
+
+
 }
