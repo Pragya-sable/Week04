7bee6b041c77124262661a0764ddb0bd39506bb2
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-13 15:23:59 +0530

    [Pragya] Solution of Efficent file copy

--- Day03/src/main/java/com/bufferedstreams/efficientfilecopy/BufferedStreamExample.java
+++ Day03/src/main/java/com/bufferedstreams/efficientfilecopy/BufferedStreamExample.java
@@ -1,4 +1,59 @@
+
 package com.bufferedstreams.efficientfilecopy;
 
+import java.io.*;
+// Program that copies a large file (e.g., 100MB) from one location to another using Buffered Streams (BufferedInputStream and BufferedOutputStream).
+// Compare the performance with normal file streams
 public class BufferedStreamExample {
+    // Method to copy a file using unbuffered streams
+    public static long copyFileUnbuffered() {
+        // calculate startTime
+        long startTime = System.nanoTime();
+        // Create FileInputStream to read the source file
+        // Create FileOutputStream to write to the destination file
+        try(FileInputStream fis = new FileInputStream("D:/Capgemini Training/input.txt");
+            FileOutputStream fos = new FileOutputStream("D:/Capgemini Training/output.txt")){
+            int byteData;
+            // Read from input file and write to the output file
+            while((byteData = fis.read()) != -1){
+                fos.write(byteData);
+            }
+            System.out.println("File copied successfully");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();;
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        // calculate endTime
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+    // Method to copy a file using buffered streams
+    public static long copyFileBuffered(){
+        // calculate startTime
+        long startTime = System.nanoTime();
+        // Create BufferedInputStream to read the source file
+        // Create BufferedOutputStream to write to the destination file
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream("D:/Capgemini Training/input.txt"));
+             BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("D:/Capgemini Training/destination.txt"))) {
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            // Read from input file and write to the destination file
+            while ((bytesRead = bis.read(buffer)) != -1) {
+                bos.write(buffer, 0, bytesRead);
+            }
+            System.out.println("File copied successfully with buffering.");
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // calculate endTime
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+
 }
+
--- Day03/src/main/java/com/bufferedstreams/efficientfilecopy/Main.java
+++ Day03/src/main/java/com/bufferedstreams/efficientfilecopy/Main.java
@@ -1,4 +1,22 @@
 package com.bufferedstreams.efficientfilecopy;
 
 public class Main {
+    public static void main(String[] args) {
+        // Measure Time for buffered file
+        long durationOfBufferedFile = BufferedStreamExample.copyFileBuffered();
+        System.out.println(" Duration of buffered file : " +durationOfBufferedFile+ " ns");
+
+        // Measure Time for unbuffered file
+        long durationOfUnBufferedFile = BufferedStreamExample.copyFileUnbuffered();
+        System.out.println(" Duration of buffered file : " +durationOfUnBufferedFile+ " ns");
+
+        // Compare
+        if(durationOfBufferedFile < durationOfUnBufferedFile ) {
+            System.out.println("Buffered file copy is fast");
+        }
+        else {
+            System.out.println("Unbuffered file copy is fast");
+        }
+    }
+
 }
