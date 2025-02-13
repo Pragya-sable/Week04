4c29886b0b3d944204b124f9923d94b38167077e
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-13 15:18:22 +0530

    [Pragya] Solution of Read Large file line by line

--- Day03/src/main/java/com/readalargefile/linebyline/LargeFileReader.java
+++ Day03/src/main/java/com/readalargefile/linebyline/LargeFileReader.java
@@ -1,4 +1,30 @@
+
 package com.readalargefile.linebyline;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+// Program that efficiently reads a large text file (500MB+)
+// line by line and prints only lines containing the word "error"
 public class LargeFileReader {
+
+    public static void main(String[] args) {
+
+        // Use try-with-resources to ensure BufferedReader is closed after use
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/ASUS/Downloads/Unit 4 - Data Structure - www.rgpvnotes.in.pdf"))) {
+            String line;
+
+            // Read the file line by line
+            while ((line = br.readLine()) != null) {
+                // Convert the line to lowercase and check if it contains "error"
+                if (line.toLowerCase().contains("error")) {
+                    System.out.println(line);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
