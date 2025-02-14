cfe2ea479c397d4860fc5405090a384a17360e50
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-14 09:44:11 +0530

    [Pragya] Test of Pipestream

--- Day03/src/test/java/pipestream/PipedStreamCommunicationTest.java
+++ Day03/src/test/java/pipestream/PipedStreamCommunicationTest.java
@@ -1,4 +1,41 @@
 package pipestream;
+import com.pipedstream.interthreadcommunication.ReaderThread;
+import com.pipedstream.interthreadcommunication.WriterThread;
+import org.junit.jupiter.api.*;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class PipedStreamCommunicationTest {
+class PipedStreamCommunicationTest {
+
+        private PipedOutputStream pos;
+        private PipedInputStream pis;
+        private WriterThread writer;
+        private ReaderThread reader;
+
+        @BeforeEach
+        void setUp() throws Exception {
+            pos = new PipedOutputStream();
+            pis = new PipedInputStream(pos);
+
+            writer = new WriterThread(pos);
+            reader = new ReaderThread(pis);
+        }
+
+        @Test
+        void testInterThreadCommunication() throws Exception {
+            writer.start();
+            reader.start();
+
+            writer.join();
+            reader.join();
+
+            assertNotNull(pis);
+            assertNotNull(pos);
+        }
+
+        @AfterEach
+        void tearDown() throws Exception {
+            pos.close();
+            pis.close();
+        }
 }
