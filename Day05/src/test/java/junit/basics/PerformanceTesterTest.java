9f33715498dfc8356e01490e93787f018f9daeef
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-15 22:35:39 +0530

    [Pragya] Add. Performance Tester

--- Day05/src/test/java/junit/basics/PerformanceTesterTest.java
+++ Day05/src/test/java/junit/basics/PerformanceTesterTest.java
@@ -1,4 +1,15 @@
 package junit.basics;
 
-public class PerformanceTesterTest {
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
+
+class PerformanceTesterTest {
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    void testLongRunningTaskTimeout() {
+        PerformanceTester.longRunningTask();
+    }
 }
