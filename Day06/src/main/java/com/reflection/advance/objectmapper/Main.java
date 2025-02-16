b8c23b995552aaadf5bbb802e6e816f27793225f
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-16 20:13:12 +0530

    [Pragya] Add.Object Mapper

---
+++ Day06/src/main/java/com/reflection/advance/objectmapper/Main.java
@@ -0,0 +1,21 @@
+package com.reflection.advance.objectmapper;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, Object> personData = new HashMap<>();
+        personData.put("name", "John Doe");
+        personData.put("age", 30);
+        personData.put("city", "New York");
+
+
+        Person person = ObjectMapper.toObject(Person.class, personData);
+
+
+        System.out.println(person);
+    }
+}
