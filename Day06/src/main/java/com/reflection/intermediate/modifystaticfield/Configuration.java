79837e6a3bf5044873c4cd2837db18cd0eefc85a
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-16 19:41:17 +0530

    [Pragya] Add.Modify Static Field

---
+++ Day06/src/main/java/com/reflection/intermediate/modifystaticfield/Configuration.java
@@ -0,0 +1,10 @@
+package com.reflection.intermediate.modifystaticfield;
+
+
+public class Configuration {
+    private static String API_KEY = "INITIAL_KEY";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
