5b4a5f4d9748f0c4153b7e53d456628544c69f0b
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-16 17:03:01 +0530

    [Pragya] Add.Todo

---
+++ Day06/src/main/java/com/annotation/practiceproblems/beginner/todo/TaskManager.java
@@ -0,0 +1,18 @@
+package com.annotation.practiceproblems.beginner.todo;
+
+public class TaskManager {
+    @Todo(task = "Implement authentication", assignedTo = "Alice", priority = "HIGH")
+    public void implementAuth() {
+        System.out.println("Implementing authentication...");
+    }
+
+    @Todo(task = "Optimize database queries", assignedTo = "Bob")
+    public void optimizeDatabase() {
+        System.out.println("Optimizing database queries...");
+    }
+
+    @Todo(task = "Refactor legacy code", assignedTo = "Charlie", priority = "LOW")
+    public void refactorCode() {
+        System.out.println("Refactoring legacy code...");
+    }
+}
