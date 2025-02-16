8e3c3a6c24240ba3a0f273c605d3a1d6f9f62bcb
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-16 16:47:50 +0530

    [Pragya] Add.Repeatable Annotation

---
+++ Day06/src/main/java/com/annotation/excercise/repeatableannotation/BugReportProcessor.java
@@ -0,0 +1,21 @@
+package com.annotation.excercise.repeatableannotation;
+import java.lang.reflect.Method;
+public class BugReportProcessor {
+    public static void main(String[] args) {
+        try {
+            Method method = BugTracker.class.getMethod("processTask");
+
+            if (method.isAnnotationPresent(BugReports.class)) {
+                BugReports bugReports = method.getAnnotation(BugReports.class);
+
+                for (BugReport bug : bugReports.value()) {
+                    System.out.println("Bug Report: " + bug.description());
+                }
+            }
+            new BugTracker().processTask();
+
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
